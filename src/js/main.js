f5c03ef7f4cc8868b4ff2c697cd7a3eb6d67b377
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-07-14 16:06:13 +0200

    fix: variables JS ES6

--- src/js/main.js
+++ src/js/main.js
@@ -47,11 +47,11 @@ button[2].addEventListener('click', () => {
     }
 });
 // button svg filter
-var turbVal = {
+const turbVal = {
     val: 0.000001
 };
-var turb = document.querySelectorAll('#filter feTurbulence')[0];
-var btTl = new TimelineLite({
+const turb = document.querySelectorAll('#filter feTurbulence')[0];
+const btTl = new TimelineLite({
     paused: true,
     onUpdate: function () {
         turb.setAttribute('baseFrequency', '0 ' + turbVal.val);
